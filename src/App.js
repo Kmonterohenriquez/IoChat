23c3062d993630df7784379c043a2623acb86959
Kevin Montero <kevinwilding1997@gmail.com>
2019-12-15 00:23:16 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import route  from './router'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {route}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Auth/Auth.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Auth =()=>{
+    return(
+        <div className="Auth">
+            <div className="Auth-container container">
+                <div className="Auth-left-side">
+                    <h1>IoChat</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit beatae, alias ipsum aliquam provident quo ex commodi animi eius minus! Facere dignissimos et molestiae accusamus exercitationem nam excepturi error distinctio.</p>
+                    <img src="" alt=""/>
+                </div>
+                <div className="Auth-right-side">
+                    
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Auth
